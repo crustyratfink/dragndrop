2e93c5dd051e3367c8da02618a8681e2e6061822
Rian Schmidt <rian.schmidt@gmail.com>
2021-09-18 15:02:13 -0700

    add allocations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { css, Global } from "@emotion/react";
 import styled from "@emotion/styled";
-import { ArrowDownward } from "@material-ui/icons";
+import { Button } from "@material-ui/core";
+import { ArrowDownward, FormatAlignRightSharp } from "@material-ui/icons";
 import clsx from "clsx";
 import React, { useEffect, useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
@@ -9,8 +10,80 @@ const dimensions = [
   { id: "strategy", content: "Strategy", color: "rgba(255, 0, 0, 0.12)" },
   { id: "publisher", content: "Publisher", color: "rgba(0, 255, 0, 0.12)" },
   { id: "channel", content: "Channel", color: "rgba(255, 255, 0, 0.12)" },
-  { id: "content", content: "Content", color: "rgba(0, 0, 255, 0.12)" },
+  { id: "tactic", content: "Tactic", color: "rgba(0, 0, 255, 0.12)" },
 ];
+
+const strategies = ["lower funnel", "mid funnel", "upper funnel"];
+const tactics = ["retargeting", "prospecting"];
+const publishers = ["trade desk", "google", "taboola"];
+const channels = ["display", "video"];
+
+const allocations = [
+  {
+    strategy: "lower funnel",
+    publisher: "taboola",
+    tactic: "retargeting",
+    channel: "display",
+    amount: 1000,
+  },
+  {
+    strategy: "mid funnel",
+    publisher: "google",
+    tactic: "prospecting",
+    channel: "video",
+    amount: 1000,
+  },
+  {
+    strategy: "upper funnel",
+    publisher: "trade desk",
+    tactic: "retargeting",
+    channel: "display",
+    amount: 1000,
+  },
+  {
+    strategy: "lower funnel",
+    publisher: "google",
+    tactic: "retargeting",
+    channel: "video",
+    amount: 1000,
+  },
+  {
+    strategy: "mid funnel",
+    publisher: "trade desk",
+    tactic: "prospecting",
+    channel: "video",
+    amount: 1000,
+  },
+  {
+    strategy: "upper funnel",
+    publisher: "google",
+    tactic: "retargeting",
+    channel: "display",
+    amount: 1000,
+  },
+  {
+    strategy: "upper funnel",
+    publisher: "trade desk",
+    tactic: "prospecting",
+    channel: "display",
+    amount: 1000,
+  },
+  {
+    strategy: "lower funnel",
+    publisher: "taboola",
+    tactic: "retargeting",
+    channel: "display",
+    amount: 1000,
+  },
+];
+
+const aggregate = (allocations: any[], field: string) => {
+  const aggs = allocations.reduce((agg: any, alloc: any) => {
+    agg[alloc[field]] = (agg[alloc[field]] ?? 0) + alloc.amount;
+    return agg;
+  }, {});
+  return aggs;
+};
 
 // a little function to help us with reordering the result
 const reorder = (list: any[], startIndex: number, endIndex: number) => {
@@ -45,11 +118,14 @@ const getListStyle = (isDraggingOver: boolean) => ({
 });
 
 const App = (props: any) => {
+  aggregate(allocations, "publisher");
   const [items, setItems] = useState<any>([]);
+  const [reset, setReset] = useState<boolean>(false);
   const [disappear, setDisappear] = useState<boolean>(false);
   useEffect(() => {
     setItems(dimensions);
-  }, []);
+    setReset(false);
+  }, [reset]);
 
   const onDragStart = () => {
     setDisappear(true);
@@ -81,7 +157,7 @@ const App = (props: any) => {
     justify-content: space-around;
     align-items: center;
     height: 5em;
-    width: 5em;
+    width: 8em;
     padding: 1em;
     box-shadow: 3px 4px 6px rgba(33, 33, 33, 0.7);
   `;
@@ -89,7 +165,6 @@ const App = (props: any) => {
   // Normally you would want to split things out into separate components.
   // But in this example everything is just done in one place for simplicity
   if (!items) return null;
-  console.log(items);
   return (
     <>
       <Global
@@ -118,6 +193,14 @@ const App = (props: any) => {
                 style={getListStyle(snapshot.isDraggingOver)}
                 {...provided.droppableProps}
               >
+                <Button
+                  style={{ marginRight: 10 }}
+                  variant="outlined"
+                  color="primary"
+                  onClick={() => setReset(true)}
+                >
+                  Reset
+                </Button>
                 {items.map((item: any, index: number) => (
                   <Draggable key={item.id} draggableId={item.id} index={index}>
                     {(provided: any, snapshot: any) => (
@@ -141,25 +224,35 @@ const App = (props: any) => {
           )}
         </Droppable>
       </DragDropContext>
+
       <div
         className={clsx([disappear && "invisible"])}
         style={{
           display: "flex",
           flexDirection: "column",
-          alignItems: 'center',
+          alignItems: "center",
           transition: "opacity .25s",
           margin: "0 auto",
-          width: '5em',
         }}
       >
         {items.map((item: any, key: number) => {
+          const aggs = aggregate(allocations, item.id);
           return (
-            <>
-              <DimensionBlock style={{ backgroundColor: item.color }}>
-                {item.content}
-              </DimensionBlock>
-              {key < (items.length - 1) && <ArrowDownward fontSize="large" />}
-            </>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              {Object.keys(aggs).map((agg: any) => (
+                <DimensionBlock
+                  style={{
+                    backgroundColor: item.color,
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <div>{item.content}</div>
+                  <div>{agg}</div>
+                  <div>${aggs[agg]}</div>
+                </DimensionBlock>
+              ))}
+            </div>
           );
         })}
       </div>
@@ -168,3 +261,5 @@ const App = (props: any) => {
 };
 
 export default App;
+
+//{//key < items.length - 1 && <ArrowDownward fontSize="large" />}
