d21bc9510122df95e74f3ec9d99f7092c15949b4
Rian Schmidt <rian.schmidt@gmail.com>
2021-09-20 13:19:51 -0700

    fiddle with a form to add values

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
 import { css, Global } from "@emotion/react";
 import styled from "@emotion/styled";
-import { Button } from "@material-ui/core";
+import {
+  Button,
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+} from "@material-ui/core";
 import { CancelOutlined } from "@material-ui/icons";
 import clsx from "clsx";
 import React, { useEffect, useState } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { Tree, TreeNode } from "react-organizational-chart";
+import { ObjectFlags } from "typescript";
 
-const dimensions = [
-  { id: "strategy", content: "Strategy", color: "rgba(255, 0, 0, 0.12)" },
-  { id: "publisher", content: "Publisher", color: "rgba(0, 255, 0, 0.12)" },
-  { id: "channel", content: "Channel", color: "rgba(255, 255, 0, 0.12)" },
-  { id: "tactic", content: "Tactic", color: "rgba(0, 0, 255, 0.12)" },
-];
+const dimensions:{[k:string]:any} = {
+  strategy: {
+    content: "Strategy",
+    color: "rgba(255, 0, 0, 0.12)",
+    options: ["lower funnel", "mid funnel", "upper funnel"],
+  },
+  publisher: {
+    content: "Publisher",
+    color: "rgba(0, 255, 0, 0.12)",
+    options: ["trade desk", "google", "taboola"],
+  },
+  channel: {
+    content: "Channel",
+    color: "rgba(255, 255, 0, 0.12)",
+    options: ["display", "video"],
+  },
+  tactic: {
+    content: "Tactic",
+    color: "rgba(0, 0, 255, 0.12)",
+    options: ["retargeting", "prospecting"],
+  },
+};
 
-// const strategies = ["lower funnel", "mid funnel", "upper funnel"];
-// const tactics = ["retargeting", "prospecting"];
-// const publishers = ["trade desk", "google", "taboola"];
-// const channels = ["display", "video"];
-
-const allocations = [
+const sampleAllocations = [
   {
     strategy: "lower funnel",
     publisher: "taboola",
@@ -111,11 +131,13 @@ const aggregate = (allocations: any[], field: string = "total") => {
   return aggs;
 };
 
-const reorder = (list: any[], startIndex: number, endIndex: number) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-  return result;
+const reorder = (list: any, startIndex: number, endIndex: number) => {
+  const keys = Object.keys(list);
+  const [removed] = keys.splice(startIndex, 1);
+  keys.splice(endIndex, 0, removed);
+  const newItems:{[k:string]:any} = {};
+  keys.forEach((k:string) => newItems[k]=dimensions[k]);
+  return newItems;
 };
 
 const grid = 8;
@@ -146,17 +168,20 @@ const getListStyle = (isDraggingOver: boolean) => ({
 });
 
 const App = () => {
-  aggregate(allocations, "publisher");
-  const [items, setItems] = useState<any>([]);
+  const [items, setItems] = useState<any>(dimensions);
+  const [data, setData] = useState<any>({});
+  const [allocations, setAllocations] = useState<any>(sampleAllocations);
   const [reset, setReset] = useState<boolean>(false);
   const [disappear, setDisappear] = useState<boolean>(false);
   useEffect(() => {
     setItems(dimensions);
+    setAllocations(sampleAllocations);
     setReset(false);
   }, [reset]);
 
   const shutUp = (itemId: string) => {
-    const newItems = items.filter((item: any) => item.id != itemId);
+    const newItems = JSON.parse(JSON.stringify(items))
+    delete newItems[itemId];
     setDisappear(false);
     setItems(newItems);
   };
@@ -179,11 +204,12 @@ const App = () => {
     setItems(newItems);
   };
 
-  const buildTree = (allocs: any, levels: any) => {
+  const buildTree = (allocs: any, levels: any[]) => {
     if (levels.length === 0) return;
     const newLevels = JSON.parse(JSON.stringify(levels));
     const thisLevel = newLevels.shift();
-    const aggs = aggregate(allocs, thisLevel.id);
+    const aggs = aggregate(allocs, thisLevel);
+    console.log(aggs);
     const dimVals = Object.keys(aggs);
     const results: any = {};
     const result: any = {};
@@ -191,28 +217,62 @@ const App = () => {
       results[dv] = {};
       results[dv].root = aggs[dv];
       results[dv].children = buildTree(
-        allocs.filter((alloc: any) => alloc[thisLevel.id] === dv),
+        allocs.filter((alloc: any) => alloc[thisLevel!] === dv),
         newLevels
       );
     });
-    result[thisLevel.id] = results;
+    result[thisLevel!] = results;
+    console.log(result);
     return result;
   };
 
-  const TreeItem = ({ level, item, value }: any) => (
-    <DimensionBlock
-      style={{
-        backgroundColor: dimensions.filter((dim: any) => dim.id === level)[0]
-          .color,
-      }}
-    >
-      <DimensionBlockInner>
-        <div>{level}</div>
-        <div>{item}</div>
-        <div>${value}</div>
-      </DimensionBlockInner>
-    </DimensionBlock>
-  );
+  const TreeItem = ({ level, item, value }: any) => {
+    console.log(level, item, value);
+    return (
+      <DimensionBlock
+        style={{
+          //@ts-ignore
+          backgroundColor: dimensions[level].color,
+        }}
+      >
+        <DimensionBlockInner>
+          <div>{level}</div>
+          <div>{item}</div>
+          <div>${value}</div>
+        </DimensionBlockInner>
+      </DimensionBlock>
+    );
+  };
+
+  const handleSubmit = () => {
+    console.log(data)
+    if (
+      data.strategy &&
+      data.tactic &&
+      data.channel &&
+      data.publisher &&
+      data.amount
+    ) {
+      console.log("Setting allocations...", data)
+      setAllocations([
+        ...allocations,
+        {
+          strategy: data.strategy,
+          publisher: data.publisher,
+          tactic: data.tactic,
+          channel: data.channel,
+          amount: data.amount,
+        },
+      ]);
+    }
+  };
+
+  const addData = (e: any) => {
+    var value;
+    if(e.target.name==="amount") {value=parseInt(e.target.value)} else {value=e.target.value};
+    setData({ ...data, [e.target.name]:  value});
+  };
+  console.log(data);
 
   const TreeLevel = ({ level, items }: any) => {
     return (
@@ -242,8 +302,9 @@ const App = () => {
   };
 
   if (!items) return null;
-  const tree = buildTree(allocations, items);
+  const tree = buildTree(allocations, Object.keys(items));
   const rootLevel = tree ? Object.keys(tree)[0] : "";
+  console.log(items)
   return (
     <>
       <Global
@@ -280,8 +341,9 @@ const App = () => {
                 >
                   Reset
                 </Button>
-                {items.map((item: any, index: number) => (
-                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                {Object.keys(items).map((item: string, index: number) => {
+                  console.log(items, item)
+                  return(<Draggable key={item} draggableId={item} index={index}>
                     {(provided: any, snapshot: any) => (
                       <div
                         ref={provided.innerRef}
@@ -292,12 +354,12 @@ const App = () => {
                           provided.draggableProps.style
                         )}
                       >
-                        {item.content}
-                        <CancelOutlined onClick={() => shutUp(item.id)} />
+                        {items[item].content}
+                        <CancelOutlined onClick={() => shutUp(item)} />
                       </div>
                     )}
                   </Draggable>
-                ))}
+                )})}
                 {provided.placeholder}
               </div>
             </div>
@@ -341,6 +403,36 @@ const App = () => {
           </Tree>
         )}
       </div>
+
+      <form onSubmit={() => console.log("whatever")}>
+        <div
+          style={{
+            width: "80vw",
+            margin: "40px auto",
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr",
+            gap: 20,
+          }}
+        >
+          {Object.keys(dimensions).map((dim: string) => (
+            <FormControl fullWidth>
+              <InputLabel htmlFor={dim}>{dim}</InputLabel>
+              <Select name={dim} id={dim} onChange={(e) => addData(e)}>
+                {/* @ts-ignore */}
+                {dimensions[dim].options.map((opt) => (
+                  <MenuItem value={opt}>{opt}</MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+          ))}
+          <FormControl fullWidth>
+            <TextField id="amount" name="amount" type="number" helperText="Amount" placeholder="Amount" onBlur={(e) => {if(e.target.value) {addData(e)}}} />
+          </FormControl>
+          <Button color="primary" onClick={handleSubmit} variant="outlined">
+            Add Allocation
+          </Button>
+        </div>
+      </form>
     </>
   );
 };
