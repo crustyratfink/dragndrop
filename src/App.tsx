5a8868ed4feb67d0cc31a740c97f5a715b0c9160
Rian Schmidt <rian.schmidt@gmail.com>
2021-09-22 14:58:32 -0700

    moar

--- src/App.tsx
+++ src/App.tsx
@@ -1,171 +1,13 @@
 import { css, Global } from "@emotion/react";
-import styled from "@emotion/styled";
-import {
-  Button,
-  FormControl,
-  FormHelperText,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-} from "@material-ui/core";
-import { CancelOutlined } from "@material-ui/icons";
 import clsx from "clsx";
 import React, { useEffect, useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import { Tree, TreeNode } from "react-organizational-chart";
-import { ObjectFlags } from "typescript";
-
-const dimensions:{[k:string]:any} = {
-  strategy: {
-    content: "Strategy",
-    color: "rgba(255, 0, 0, 0.12)",
-    options: ["lower funnel", "mid funnel", "upper funnel"],
-  },
-  publisher: {
-    content: "Publisher",
-    color: "rgba(0, 255, 0, 0.12)",
-    options: ["trade desk", "google", "taboola"],
-  },
-  channel: {
-    content: "Channel",
-    color: "rgba(255, 255, 0, 0.12)",
-    options: ["display", "video"],
-  },
-  tactic: {
-    content: "Tactic",
-    color: "rgba(0, 0, 255, 0.12)",
-    options: ["retargeting", "prospecting"],
-  },
-};
-
-const sampleAllocations = [
-  {
-    strategy: "lower funnel",
-    publisher: "taboola",
-    tactic: "retargeting",
-    channel: "display",
-    amount: 1000,
-  },
-  {
-    strategy: "mid funnel",
-    publisher: "google",
-    tactic: "prospecting",
-    channel: "video",
-    amount: 1000,
-  },
-  {
-    strategy: "upper funnel",
-    publisher: "trade desk",
-    tactic: "retargeting",
-    channel: "display",
-    amount: 1000,
-  },
-  {
-    strategy: "lower funnel",
-    publisher: "google",
-    tactic: "retargeting",
-    channel: "video",
-    amount: 1000,
-  },
-  {
-    strategy: "mid funnel",
-    publisher: "trade desk",
-    tactic: "prospecting",
-    channel: "video",
-    amount: 1000,
-  },
-  {
-    strategy: "upper funnel",
-    publisher: "google",
-    tactic: "retargeting",
-    channel: "display",
-    amount: 1000,
-  },
-  {
-    strategy: "upper funnel",
-    publisher: "trade desk",
-    tactic: "prospecting",
-    channel: "display",
-    amount: 1000,
-  },
-  {
-    strategy: "lower funnel",
-    publisher: "taboola",
-    tactic: "retargeting",
-    channel: "display",
-    amount: 1000,
-  },
-];
-
-const DimensionBlock = styled.div`
-  display: inline-block;
-  height: 5em;
-  width: 8em;
-  box-shadow: 3px 4px 6px rgba(33, 33, 33, 0.7);
-`;
-
-const DimensionBlockInner = styled.div`
-  text-decoration: none;
-  padding: 1em;
-  box-sizing: border-box;
-  width: 100%;
-  height: 100%;
-  color: #000;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
-  background-color: inherit;
-`;
-
-const aggregate = (allocations: any[], field: string = "total") => {
-  const aggs = allocations.reduce((agg: any, alloc: any) => {
-    if (field === "total") {
-      agg.total = (agg.total ?? 0) + alloc.amount;
-    } else {
-      agg[alloc[field]] = (agg[alloc[field]] ?? 0) + alloc.amount;
-    }
-    return agg;
-  }, {});
-  return aggs;
-};
-
-const reorder = (list: any, startIndex: number, endIndex: number) => {
-  const keys = Object.keys(list);
-  const [removed] = keys.splice(startIndex, 1);
-  keys.splice(endIndex, 0, removed);
-  const newItems:{[k:string]:any} = {};
-  keys.forEach((k:string) => newItems[k]=dimensions[k]);
-  return newItems;
-};
-
-const grid = 8;
-
-const getItemStyle = (isDragging: boolean, draggableStyle: any) => ({
-  userSelect: "none",
-  padding: `${grid}px ${grid * 2}px ${grid}px ${grid * 2}px`,
-  margin: `0 ${grid}px 0 0`,
-  borderRadius: 999,
-  color: "white",
-
-  // change background colour if dragging
-  background: isDragging ? "darkorchid" : "indigo",
-  display: "flex",
-  flexDirection: "row",
-  alignItems: "center",
-  gap: 20,
-
-  // styles we need to apply on draggables
-  ...draggableStyle,
-});
-
-const getListStyle = (isDraggingOver: boolean) => ({
-  background: isDraggingOver ? "lightblue" : "lightgrey",
-  display: "flex",
-  padding: grid,
-  overflow: "auto",
-});
+import { Allocation, AllocationType } from "./components/Allocation";
+import AllocationForm from "./components/AllocationForm";
+import DimensionBlock from "./components/DimensionBlock";
+import DragDrop from "./components/DragDrop";
+import { sampleAllocations, dimensions } from "./lib/data";
+import { aggregate, buildTree } from "./lib/helpers";
 
 const App = () => {
   const [items, setItems] = useState<any>(dimensions);
@@ -179,73 +21,35 @@ const App = () => {
     setReset(false);
   }, [reset]);
 
-  const shutUp = (itemId: string) => {
-    const newItems = JSON.parse(JSON.stringify(items))
-    delete newItems[itemId];
-    setDisappear(false);
-    setItems(newItems);
-  };
-
-  const onDragStart = () => {
-    setDisappear(true);
-  };
-
-  const onDragEnd = (result: any) => {
-    // dropped outside the list
-    if (!result.destination) {
-      return;
-    }
-    const newItems = reorder(
-      items,
-      result.source.index,
-      result.destination.index
+  const deleteAllocation = (target: AllocationType) => {
+    console.log(target);
+    setAllocations(
+      allocations.filter((alloc: AllocationType) => {
+        const pass =
+          !(target.strategy === alloc.strategy &&
+          target.publisher === alloc.publisher &&
+          target.tactic === alloc.tactic &&
+          target.channel === alloc.channel &&
+          target.amount === alloc.amount);
+          console.log(pass)
+        return pass;
+      })
     );
-    setDisappear(false);
-    setItems(newItems);
-  };
-
-  const buildTree = (allocs: any, levels: any[]) => {
-    if (levels.length === 0) return;
-    const newLevels = JSON.parse(JSON.stringify(levels));
-    const thisLevel = newLevels.shift();
-    const aggs = aggregate(allocs, thisLevel);
-    console.log(aggs);
-    const dimVals = Object.keys(aggs);
-    const results: any = {};
-    const result: any = {};
-    dimVals.forEach((dv: string) => {
-      results[dv] = {};
-      results[dv].root = aggs[dv];
-      results[dv].children = buildTree(
-        allocs.filter((alloc: any) => alloc[thisLevel!] === dv),
-        newLevels
-      );
-    });
-    result[thisLevel!] = results;
-    console.log(result);
-    return result;
   };
 
   const TreeItem = ({ level, item, value }: any) => {
-    console.log(level, item, value);
     return (
       <DimensionBlock
-        style={{
-          //@ts-ignore
-          backgroundColor: dimensions[level].color,
-        }}
-      >
-        <DimensionBlockInner>
-          <div>{level}</div>
-          <div>{item}</div>
-          <div>${value}</div>
-        </DimensionBlockInner>
-      </DimensionBlock>
+        color={dimensions[level].color}
+        level={level}
+        item={item}
+        value={value}
+      />
     );
   };
 
   const handleSubmit = () => {
-    console.log(data)
+    console.log(data);
     if (
       data.strategy &&
       data.tactic &&
@@ -253,7 +57,7 @@ const App = () => {
       data.publisher &&
       data.amount
     ) {
-      console.log("Setting allocations...", data)
+      console.log("Setting allocations...", data);
       setAllocations([
         ...allocations,
         {
@@ -264,14 +68,18 @@ const App = () => {
           amount: data.amount,
         },
       ]);
-      setData({})
+      setData({});
     }
   };
 
   const addData = (e: any) => {
     var value;
-    if(e.target.name==="amount") {value=parseInt(e.target.value)} else {value=e.target.value};
-    setData({ ...data, [e.target.name]:  value});
+    if (e.target.name === "amount") {
+      value = parseInt(e.target.value);
+    } else {
+      value = e.target.value;
+    }
+    setData({ ...data, [e.target.name]: value });
   };
   console.log(data);
 
@@ -305,136 +113,80 @@ const App = () => {
   if (!items) return null;
   const tree = buildTree(allocations, Object.keys(items));
   const rootLevel = tree ? Object.keys(tree)[0] : "";
-  console.log(items)
   return (
-    <>
-      <Global
-        styles={css`
-          .invisible {
-            opacity: 0.1 !important;
-          }
-          .visible {
-            opacity: 1;
-          }
-        `}
-      />
-      <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
-        <Droppable droppableId="droppable" direction="horizontal">
-          {(provided: any, snapshot: any) => (
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                margin: "60px auto 60px 0",
-                justifyContent: "space-around",
-              }}
-            >
-              <div
-                ref={provided.innerRef}
-                style={getListStyle(snapshot.isDraggingOver)}
-                {...provided.droppableProps}
-              >
-                <Button
-                  style={{ marginRight: 10 }}
-                  variant="outlined"
-                  color="primary"
-                  onClick={() => setReset(true)}
-                >
-                  Reset
-                </Button>
-                {Object.keys(items).map((item: string, index: number) => {
-                  console.log(items, item)
-                  return(<Draggable key={item} draggableId={item} index={index}>
-                    {(provided: any, snapshot: any) => (
-                      <div
-                        ref={provided.innerRef}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        style={getItemStyle(
-                          snapshot.isDragging,
-                          provided.draggableProps.style
-                        )}
-                      >
-                        {items[item].content}
-                        <CancelOutlined onClick={() => shutUp(item)} />
-                      </div>
-                    )}
-                  </Draggable>
-                )})}
-                {provided.placeholder}
-              </div>
-            </div>
-          )}
-        </Droppable>
-      </DragDropContext>
-
-      <div
-        className={clsx([disappear && "invisible"])}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          transition: "opacity .25s",
-          margin: "0 auto",
-        }}
-      >
-        {tree && (
-          <Tree
-            lineWidth={"2px"}
-            lineColor={"green"}
-            lineBorderRadius={"10px"}
-            label={
-              <DimensionBlock
-                style={{
-                  backgroundColor: "lightgray",
-                  display: "flex",
-                  flexDirection: "column",
-                  margin: "0 auto",
-                }}
-              >
-                <DimensionBlockInner>
-                  <div>Campaign</div>
-                  <div>Name</div>
-                  <div>${aggregate(allocations).total}</div>
-                </DimensionBlockInner>
-              </DimensionBlock>
+    <div style={{ display: "grid", gridTemplateColumns: "auto 15%" }}>
+      <div>
+        <Global
+          styles={css`
+            .invisible {
+              opacity: 0.1 !important;
             }
-          >
-            <TreeLevel level={rootLevel} items={tree[rootLevel]} />
-          </Tree>
-        )}
-      </div>
-
-      <form onSubmit={() => console.log("whatever")}>
+            .visible {
+              opacity: 1;
+            }
+          `}
+        />
+        <DragDrop
+          items={items}
+          setItems={setItems}
+          dimensions={dimensions}
+          setDisappear={setDisappear}
+          setReset={setReset}
+        />
         <div
+          className={clsx([disappear && "invisible"])}
           style={{
-            width: "80vw",
-            margin: "40px auto",
-            display: "grid",
-            gridTemplateColumns: "1fr 1fr 1fr",
-            gap: 20,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            transition: "opacity .25s",
+            margin: "0 auto",
           }}
         >
-          {Object.keys(dimensions).map((dim: string) => (
-            <FormControl fullWidth>
-              <InputLabel htmlFor={dim}>{dim}</InputLabel>
-              <Select name={dim} id={dim} value={data[dim] || ''} onChange={(e) => addData(e)}>
-                {/* @ts-ignore */}
-                {dimensions[dim].options.map((opt) => (
-                  <MenuItem value={opt}>{opt}</MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-          ))}
-          <FormControl fullWidth>
-            <TextField id="amount" value={data.amount || ''} name="amount" type="number" helperText="Amount" placeholder="Amount" onChange={(e) => {if(e.target.value) {addData(e)}}} />
-          </FormControl>
-          <Button color="primary" onClick={handleSubmit} variant="outlined">
-            Add Allocation
-          </Button>
+          {tree && (
+            <Tree
+              lineWidth={"2px"}
+              lineColor={"green"}
+              lineBorderRadius={"10px"}
+              label={
+                <DimensionBlock
+                  color="lightgray"
+                  level="Campaign"
+                  item="Name"
+                  value={aggregate(allocations).total}
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    margin: "0 auto",
+                  }}
+                />
+              }
+            >
+              <TreeLevel level={rootLevel} items={tree[rootLevel]} />
+            </Tree>
+          )}
         </div>
-      </form>
-    </>
+
+        <AllocationForm
+          dimensions={dimensions}
+          data={data}
+          addData={addData}
+          handleSubmit={handleSubmit}
+        />
+      </div>
+      <div>
+        {allocations.map((alloc: any) => {
+          return (
+            <div>
+              <Allocation
+                allocation={alloc}
+                deleteAllocation={deleteAllocation}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 };
 
